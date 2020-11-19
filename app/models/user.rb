6e1ff24162c9fdc84c903ebdba651cc85637ba42
first-star <72744812+first-star@users.noreply.github.com>
2020-11-19 19:18:52 +0900

    with_optionの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+    validates :family_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ン]+\z/ }
+    validates :birthday
+  end
 
   has_many :items
   has_many :purchases
