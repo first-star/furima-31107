3901421953a43dc8b97d4c2b543de05889169d83
first-star <72744812+first-star@users.noreply.github.com>
2020-11-23 22:09:07 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,11 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new(item_params)
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
